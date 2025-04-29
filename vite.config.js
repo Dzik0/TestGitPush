2e7fa15caea4cf001401d48c1466f657a95a0050
Piotr <hunter976@gmail.com>
2025-04-30 01:46:15 +0200

    changed the repo name

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/TestGitPush/',
+  base: '/product-list-with-cart/',
   plugins: [react()],
 });
